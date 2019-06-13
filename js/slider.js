180b5faef074f897948593fc9ead1c0d5e65c552
nebulalagoon <atramhenc@gmail.com>
2019-06-13 14:03:19 +0200

    sliding the images works, looping around doesn't

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,31 @@
 $(document).ready(()=> {
 
+    const imageRow1 = $('#row1');
+    const imageRow2 = $('#row2').find('.imageRow');
+
+    //slide left
+    $('#arrowLeft').on('click', () => {
+        let widthRow1 = imageRow1.find('.active').innerWidth();
+        let toRow1={'right': '+=' + widthRow1 + 'px'};
+        imageRow1.find('img').animate(toRow1);
+
+        let widthRow2 = imageRow2.find('.active').innerWidth();
+        let toRow2={'right': '+=' + widthRow2 + 'px'};
+        imageRow2.find('img').animate(toRow2);
+
+        }
+    );
+
+    //slide right
+    $('#arrowRight').on('click', () => {
+        let widthRow1 = imageRow1.find('.active').innerWidth();
+        let toRow1={'right': '-=' + widthRow1 + 'px'};
+        imageRow1.find('img').animate(toRow1);
+
+        let widthRow2 = imageRow2.find('.active').innerWidth();
+        let toRow2={'right': '-=' + widthRow2 + 'px'};
+        imageRow2.find('img').animate(toRow2);
+
+        }
+    );
 });
