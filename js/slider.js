13e5662cf247643c15fdce1defa79a7314fd79f9
nebulalagoon <atramhenc@gmail.com>
2019-06-13 18:12:44 +0200

    looping works, the animation doesn't

--- js/slider.js
+++ js/slider.js
@@ -5,27 +5,62 @@ $(document).ready(()=> {
 
     //slide left
     $('#arrowLeft').on('click', () => {
-        let widthRow1 = imageRow1.find('.active').innerWidth();
+        //looping images
+        let imgArray1 = imageRow1.find('img').toArray();
+        let add1 = imgArray1[0];
+        imageRow1.append(add1);
+
+        let imgArray2 = imageRow2.find('img').toArray();
+        let add2 = imgArray2[0]
+        imageRow2.append(add2);
+
+        //shift the active class
+        imageRow1.find('.active').removeClass('active');
+        imageRow1.find('img').last().addClass('active');
+
+        imageRow2.find('.active').removeClass('active');
+        imageRow2.find('img').last().addClass('active');
+
+        //animation
+        /*let widthRow1 = imageRow1.find('.active').innerWidth();
         let toRow1={'right': '+=' + widthRow1 + 'px'};
         imageRow1.find('img').animate(toRow1);
 
         let widthRow2 = imageRow2.find('.active').innerWidth();
         let toRow2={'right': '+=' + widthRow2 + 'px'};
-        imageRow2.find('img').animate(toRow2);
-
+        imageRow2.find('img').animate(toRow2);*/
+        
         }
     );
 
     //slide right
     $('#arrowRight').on('click', () => {
-        let widthRow1 = imageRow1.find('.active').innerWidth();
+
+        //looping trough images
+        let imgArray1 = imageRow1.find('img').toArray();
+        let add1 = imgArray1.pop();
+        imageRow1.prepend(add1);
+
+        let imgArray2 = imageRow2.find('img').toArray();
+        let add2 = imgArray2.pop();
+        imageRow2.prepend(add2);
+
+        
+        //shift the active class
+        imageRow1.find('.active').removeClass('active');
+        imageRow1.find('img').last().addClass('active');
+
+        imageRow2.find('.active').removeClass('active');
+        imageRow2.find('img').last().addClass('active');
+        
+        //animation
+        /*let widthRow1 = imageRow1.find('.active').innerWidth();
         let toRow1={'right': '-=' + widthRow1 + 'px'};
         imageRow1.find('img').animate(toRow1);
 
         let widthRow2 = imageRow2.find('.active').innerWidth();
         let toRow2={'right': '-=' + widthRow2 + 'px'};
-        imageRow2.find('img').animate(toRow2);
-
-        }
-    );
+        imageRow2.find('img').animate(toRow2);*/
+        
+    });
 });
