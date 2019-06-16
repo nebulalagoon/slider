3d7549df4f9ad321d6f7df56bb3903e03030221f
nebulalagoon <atramhenc@gmail.com>
2019-06-16 20:53:09 +0200

    rewrite start

--- js/slider.js
+++ js/slider.js
@@ -5,64 +5,12 @@ $(document).ready(()=> {
 
     //slide left
     $('#arrowLeft').on('click', () => {
-
-        //row1
-        let widthRow1 = imageRow1.find('.first').innerWidth();
-        let imgArray1 = imageRow1.find('img').toArray();
-
-        imageRow1.append(imgArray1[0]);
-        imageRow1.find('img').last().css('right', '-' + widthRow1 + 'px');
         
-        moveLeft(imageRow1.find('img').last(), widthRow1);
-        
-        moveClassFirst(imageRow1);
-        moveClassLast(imageRow1);
-
-        //row 2
-        let widthRow2 = imageRow2.find('.first').innerWidth();
-        let imgArray2 = imageRow2.find('img').toArray();
-        let add2 = imgArray2[0];
-
-        imageRow2.append(add2);
-        imageRow2.find('img').last().css('right', '-' + widthRow2 + 'px');
-
-
-        moveLeft(imageRow2.find('img').last(), widthRow2);
-
-        moveClassFirst(imageRow2);
-        moveClassLast(imageRow2);
-        }
-    );
+        });
 
     //slide right
     $('#arrowRight').on('click', () => {
-
-        //row 1
-        let imgArray1 = imageRow1.find('img').toArray();
-        let widthRow1 = imageRow1.find('.last').innerWidth();
-
-        let add1 = imgArray1.pop();
-        imageRow1.prepend(add1);
-
-        imageRow1.find('img').first().css('right', widthRow1 + 'px');
-
-        moveRight(imageRow1.find('img').last(), widthRow1);
         
-        moveClassFirst(imageRow1);
-        moveClassLast(imageRow1);
-
-        //row 2
-        let imgArray2 = imageRow2.find('img').toArray();
-        let widthRow2 = imageRow2.find('.last').innerWidth();
-
-        let add2 = imgArray2.pop();
-        imageRow2.prepend(add2);
-
-        imageRow2.find('img').last().css('right', widthRow2 + 'px');
-        moveRight(imageRow2.find('img').last(), widthRow2);
-
-        moveClassFirst(imageRow2);
-        moveClassLast(imageRow2);
     });
 });
 
