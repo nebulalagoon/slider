77758d2e94b812a870ba7e559f21786a0c2bda2d
nebulalagoon <atramhenc@gmail.com>
2019-06-16 23:35:24 +0200

    animation works, but weird

--- js/slider.js
+++ js/slider.js
@@ -9,23 +9,25 @@ $(document).ready(()=> {
         const widthRow1 = $imageRow1.find('.first').innerWidth();
         const widthRow2 = $imageRow2.find('.first').innerWidth();
    
-        const $imageArray1 = $imageRow1.find('img').toArray();
-        const $imageArray2 = $imageRow2.find('img').toArray();
+        const imageArray1 = $imageRow1.find('img').toArray();
+        const imageArray2 = $imageRow2.find('img').toArray();
 
-        moveLeft($imageRow1.find('img'), widthRow1);
-        moveLeft($imageRow2.find('img'), widthRow2);
+        /*moveLeft($imageRow1.find('img'), widthRow1);
+        moveLeft($imageRow2.find('img'), widthRow2);*/
 
-        /*this should work but it doesn't
+        let position = widthRow1;
+        
         for (let i=$imageRow1.find('img').length-1; i>0; i--) {
-            moveLeft($imageRow1.find('img')[i], $imageRow1.find('img')[i].clientWidth); => NotSupportedError: Animation to or from an underlying value is not yet supported.
-        }*/
+            moveLeft($imageRow1.find('img')[i], position); 
+            position += $imageRow1.find('img')[i].clientWidth;
+        }
 
         //row 1 move first element to end
-        $imageRow1.append($imageArray1[0]);
-        //moveLeft($imageRow1.find('img').last(), widthRow2);
+        $imageRow1.append(imageArray1[0]);
+        moveLeft($imageRow1.find('img').last(), 0);
 
         //row 2 move first element to end
-        $imageRow2.append($imageArray2[0]);
+        $imageRow2.append(imageArray2[0]);
 
         moveClassFirst($imageRow1);
         moveClassFirst($imageRow2);
@@ -36,21 +38,21 @@ $(document).ready(()=> {
         });
 
     //slide right
-    $('#arrowRight').on('click', () => {
+    jQuery('#arrowRight').on('click', () => {
         const widthRow1 = $imageRow1.find('.last').innerWidth();
         const widthRow2 = $imageRow2.find('.last').innerWidth();
      
-        const $imageArray1 = $imageRow1.find('img').toArray();
-        const $imageArray2 = $imageRow2.find('img').toArray();
+        const imageArray1 = $imageRow1.find('img').toArray();
+        const imageArray2 = $imageRow2.find('img').toArray();
 
         moveRight($imageRow1.find('img'), widthRow1);
         moveRight($imageRow2.find('img'), widthRow2);
 
         //row 1 move first element to end
-        $imageRow1.prepend($imageArray1[$imageArray1.length - 1]);
+        $imageRow1.prepend(imageArray1[imageArray1.length - 1]);
 
         //row 2 move first element to end
-        $imageRow2.prepend($imageArray2[$imageArray2.length - 1]);
+        $imageRow2.prepend(imageArray2[imageArray2.length - 1]);
 
         moveClassFirst($imageRow1);
         moveClassFirst($imageRow2);
@@ -63,20 +65,19 @@ $(document).ready(()=> {
 //helper functions
 
 function moveLeft (element, distance) {
-    console.log('moving left');
-    element.animate({'right': distance + 'px'}); //just moves more and more to the left if += is used
+    $(element).animate({'right': distance + 'px'}); //just moves more and more to the left if += is used
 }
 
 function moveRight (element, distance) {
-    element.animate({'right': distance + 'px'});
+    $(element).animate({'right': distance + 'px'});
 }
 
 function moveClassFirst (row) {
-    row.find('.first').removeClass('first');
-    row.find('img').first().addClass('first');
+    $(row).find('.first').removeClass('first');
+    $(row).find('img').first().addClass('first');
 }
 
 function moveClassLast (row) {
-    row.find('.last').removeClass('last');
-    row.find('img').last().addClass('last');
+    $(row).find('.last').removeClass('last');
+    $(row).find('img').last().addClass('last');
 }
