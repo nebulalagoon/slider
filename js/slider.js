841d3c37f79523e749335d839a54a4e30c2e69e8
nebulalagoon <atramhenc@gmail.com>
2019-06-16 19:52:39 +0200

    commit before complete rewrite

--- js/slider.js
+++ js/slider.js
@@ -5,54 +5,83 @@ $(document).ready(()=> {
 
     //slide left
     $('#arrowLeft').on('click', () => {
-        
-        //row1 adding animation
-        imageRow1.find('img').addClass('slideLeft');
-        setTimeout(() => {
-            imageRow1.find('img').removeClass('slideLeft');
-        }, 500);
 
-        //row 2 adding animation
-        imageRow2.find('img').addClass('slideLeft');
-        setTimeout(() => {
-            imageRow2.find('img').removeClass('slideLeft');
-        }, 500);
-
-        //row1 looping images
+        //row1
+        let widthRow1 = imageRow1.find('.first').innerWidth();
         let imgArray1 = imageRow1.find('img').toArray();
-        let add1 = imgArray1[0];
-        imageRow1.append(add1);
 
-        //row 2 looping images
+        imageRow1.append(imgArray1[0]);
+        imageRow1.find('img').last().css('right', '-' + widthRow1 + 'px');
+        
+        moveLeft(imageRow1.find('img').last(), widthRow1);
+        
+        moveClassFirst(imageRow1);
+        moveClassLast(imageRow1);
+
+        //row 2
+        let widthRow2 = imageRow2.find('.first').innerWidth();
         let imgArray2 = imageRow2.find('img').toArray();
         let add2 = imgArray2[0];
+
         imageRow2.append(add2);
+        imageRow2.find('img').last().css('right', '-' + widthRow2 + 'px');
+
+
+        moveLeft(imageRow2.find('img').last(), widthRow2);
+
+        moveClassFirst(imageRow2);
+        moveClassLast(imageRow2);
         }
     );
 
     //slide right
     $('#arrowRight').on('click', () => {
 
-        //row 1 adding animation
-        imageRow1.find('img').addClass('slideRight');
-        setTimeout(() => {
-            imageRow1.find('img').removeClass('slideRight');
-        }, 500);
-
-        //row 2 adding animation
-        imageRow2.find('img').addClass('slideRight'); 
-        setTimeout(() => {
-            imageRow2.find('img').removeClass('slideRight');
-        }, 500); 
-
-        //row 1 looping trough images
+        //row 1
         let imgArray1 = imageRow1.find('img').toArray();
+        let widthRow1 = imageRow1.find('.last').innerWidth();
+
         let add1 = imgArray1.pop();
         imageRow1.prepend(add1);
 
-        //row 2 looping trough images
+        imageRow1.find('img').first().css('right', widthRow1 + 'px');
+
+        moveRight(imageRow1.find('img').last(), widthRow1);
+        
+        moveClassFirst(imageRow1);
+        moveClassLast(imageRow1);
+
+        //row 2
         let imgArray2 = imageRow2.find('img').toArray();
+        let widthRow2 = imageRow2.find('.last').innerWidth();
+
         let add2 = imgArray2.pop();
         imageRow2.prepend(add2);
+
+        imageRow2.find('img').last().css('right', widthRow2 + 'px');
+        moveRight(imageRow2.find('img').last(), widthRow2);
+
+        moveClassFirst(imageRow2);
+        moveClassLast(imageRow2);
     });
 });
+
+//helper functions
+
+function moveLeft (element, distance) {
+    element.animate({'right': '+=' + distance + 'px'});
+}
+
+function moveRight (element, distance) {
+    element.animate({'right': '-=' + distance + 'px'});
+}
+
+function moveClassFirst (row) {
+    row.find('.first').removeClass('first');
+    row.find('img').first().addClass('first');
+}
+
+function moveClassLast (row) {
+    row.find('.last').removeClass('last');
+    row.find('img').last().addClass('last');
+}
